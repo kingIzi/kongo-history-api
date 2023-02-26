4452e65091b5bdedf814dbbd598464a8017342a0
Izi Kapampa <56266013+kingIzi@users.noreply.github.com>
2023-02-26 21:59:11 +0100

    401 unauthorized is blocking swagger ui

--- src/main/java/com/kongo/history/api/kongohistoryapi/utils/AppUtilities.java
+++ src/main/java/com/kongo/history/api/kongohistoryapi/utils/AppUtilities.java
@@ -12,36 +12,31 @@ import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.function.Supplier;
 
-
 public class AppUtilities {
-   
-    public static final String DATE_FORMAT = "yyyy-MM-dd";
-    
 
-    public static String convertDateToString(final Date date){
-        final var sdf=new SimpleDateFormat(AppUtilities.DATE_FORMAT);
+    public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+    public static String convertDateToString(final Date date) {
+        final var sdf = new SimpleDateFormat(AppUtilities.DATE_FORMAT);
         return sdf.format(date);
     }
 
-    public static Date convertStringFormatToDate(final String dateString) throws DateTimeParseException{
+    public static Date convertStringFormatToDate(final String dateString) throws DateTimeParseException {
         final var dateTimeFormatter = DateTimeFormatter.ofPattern(AppUtilities.DATE_FORMAT);
         final var localDate = LocalDate.parse(dateString, dateTimeFormatter);
         return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
     }
 
-    public static boolean modifiableValue(final String valueData,final String formData){
-        try{
+    public static boolean modifiableValue(final String valueData, final String formData) {
+        try {
             return !formData.isEmpty();
-        }
-        catch (NullPointerException e){
+        } catch (NullPointerException e) {
             e.printStackTrace();
         }
         return false;
     }
 
-
-
-    public static Supplier<ValueDataException> supplyException(final String message,final String code){
-        return () -> new ValueDataException(message,code);
+    public static Supplier<ValueDataException> supplyException(final String message, final String code) {
+        return () -> new ValueDataException(message, code);
     }
 }
